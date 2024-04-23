f2e2b570cd676dcee60fa5103835440953f181bd
风间 <norbocampasjb9657@gmail.com>
2024-04-23 23:37:12 +0800

    init-初始化

---
+++ src/main/java/com/xyc/usercenter/UserCenterApplication.java
@@ -0,0 +1,24 @@
+package com.xyc.usercenter;
+
+
+import org.mybatis.spring.annotation.MapperScan;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * 启动类
+ *
+ * @author xuyuyu
+
+ */
+@SpringBootApplication
+@MapperScan("com.xyc.usercenter.mapper")
+public class UserCenterApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(UserCenterApplication.class, args);
+    }
+
+}
+
+// https://github.com/Xuyuyu520
---
+++ src/main/java/com/xyc/usercenter/mapper/UserMapper.java
@@ -0,0 +1,18 @@
+package com.xyc.usercenter.mapper;
+
+import com.xyc.usercenter.model.domain.User;
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+
+/**
+ * 用户 Mapper
+ *
+ * @author xuyuyu
+
+ */
+public interface UserMapper extends BaseMapper<User> {
+
+}
+
+
+// [编程知识星球] 零基础快速入门编程，不走弯路！30+ 原创学习路线和专栏、1000+ 编程精华文章、500+ 编程学习指南、20T+ 编程资源汇总
+
